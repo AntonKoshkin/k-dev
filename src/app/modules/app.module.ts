ed66036c33e61ae987124b3e0097eef028c5ef89
scink <scink666@gmail.com>
2017-02-14 13:21:24 +0300

    сохранение

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -13,18 +13,6 @@ import {PersonalService, PortfolioService, DataFixService} from '../services';
 
 import 'hammerjs';
 
-console.assert(HttpModule, 'HttpModule')
-console.assert(NgModule, 'NgModule')
-console.assert(MaterialModule, 'MaterialModule')
-console.assert(FlexLayoutModule, 'FlexLayoutModule')
-console.assert(AppRoutingModule, 'AppRoutingModule')
-console.assert(AppComponent, 'AppComponent')
-console.assert(HomePageComponent, 'HomePageComponent')
-console.assert(PortfolioComponent, 'PortfolioComponent')
-console.assert(PersonalService, 'PersonalService')
-console.assert(PortfolioService, 'PortfolioService')
-console.assert(DataFixService, 'DataFixService')
-
 @NgModule({
 	bootstrap   : [AppComponent],
 	declarations: [
